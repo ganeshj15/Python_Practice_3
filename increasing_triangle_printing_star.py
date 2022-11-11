eaee01482f58c404dffa030238547a002b042243
Ganesh Jambhrunkar <117712010+gdj1588@users.noreply.github.com>
2022-11-11 22:21:01 +0530

    Update increasing_triangle_printing_star.py
    added the user input line and the output

--- increasing_triangle_printing_star.py
+++ increasing_triangle_printing_star.py
@@ -1,5 +1,15 @@
 n = 5 # n is required number of rows.
+# n = int(input('Enter the number of rows you want')) to get the number of rows from user use this line.
 for i in range(n):
     for j in range(i+1):
         print('*', end=' ')
     print()
+
+"""
+This O/P will be produced
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+"""
